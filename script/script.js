666e606a709aa3a68309bc4d99c61411f13a0cc7
Tim <teiem@yahoo.com>
2020-02-20 14:44:03 +0100

    fbhjdsf

---
+++ script/script.js
@@ -0,0 +1,69 @@
+const Config = {
+    name: "user",
+    Links: [
+        [
+            "site",
+            [
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"]
+            ]
+        ],
+        [
+            "site",
+            [
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"]
+            ]
+        ],
+        [
+            "site",
+            [
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"]
+            ]
+        ],
+        [
+            "site",
+            [
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"]
+            ]
+        ]
+    ]
+}
+
+const Main = (() => {
+    const list = document.getElementById("list");
+    const names = document.querySelectorAll("[data-Name]");
+    const search = document.getElementById("search");
+
+    const init = () => {
+        list.innerHTML = Config.Links.map(([gName, Links]) => `
+            <li>
+                <h1>${gName}<h1>
+                <ul>
+                    ${Links.map(([lName, url]) => `
+                        <li>
+                            <a href="${url}">${lName}</a>
+                        </li>`
+                    ).join("")}
+                </ul>
+            </li>` 
+        ).join("")
+        
+        names.forEach(el => {
+            el.innerText = Config.name;
+        });
+
+        document.addEventListener("keydown", e => e.key.length === 1 && search.focus())
+    };
+
+    return {
+        init,
+    };
+})();
+
+Main.init()
