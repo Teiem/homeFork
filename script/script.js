181e88c59d67dfbf6ea4555cd0a2b4025edacdb1
Tim <teiem@yahoo.com>
2020-02-21 05:22:23 +0100

    improved collapsing of groups

--- script/script.js
+++ script/script.js
@@ -40,19 +40,21 @@ const Main = (() => {
     const list = document.getElementById("list");
     const names = document.querySelectorAll("[data-Name]");
     const search = document.getElementById("search");
+    const main = document.getElementsByTagName("main")[0];
+    const html = document.getRootNode().children[0];
     const form = document.forms[0];
 
     const init = () => {
         list.innerHTML = Config.Links.map(([gName, Links]) => `
             <li>
                 <h1 onclick="this.parentNode.classList.toggle('hideChildren')">${gName}</h1>
-                <ul>
+                    <ul class="ulWrap" style="--linkCount: ${Links.length};">
                     ${Links.map(([lName, url]) => `
                         <li>
                             <a href="${url}">${lName}</a>
                         </li>`
                     ).join("")}
-                </ul>
+                    </ul>
             </li>` 
         ).join("")
         
@@ -62,6 +64,10 @@ const Main = (() => {
 
         document.addEventListener("keydown", e => e.key.length === 1 && search.focus());
         search.addEventListener("keydown", () => (window.event ? event.keyCode : e.which) == 13 && form.submit());
+
+        main.style.setProperty("--categorys", Config.Links.length);
+        main.style.setProperty("--totalLinks", Config.Links.reduce((prev, cur) => prev + cur[1].length, 0));
+        html.style.fontSize = Config.scale * 20 + "px";
     };
 
     return {
